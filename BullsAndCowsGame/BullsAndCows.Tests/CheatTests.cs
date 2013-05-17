c40fc7d4236637c9e4440790312a1e8f90f19885
Polina Koleva <polly.akoleva@gmail.com>
2013-05-17 15:01:08 +0300

    add class CheatTests for unit testing of class Cheat

---
+++ BullsAndCowsGame/BullsAndCows.Tests/CheatTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class CheatTests
+    {
+        [TestMethod]
+        public void GetCheatTest()
+        {
+            GameNumber number = new GameNumber(2, 4, 5, 2);
+            
+        }
+    }
+}

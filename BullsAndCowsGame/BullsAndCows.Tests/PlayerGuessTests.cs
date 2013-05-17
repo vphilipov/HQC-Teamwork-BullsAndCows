d340ad9bd4ce6838c6a8499d4f021fbd04a739f1
vphilipov <vess.ph@gmail.com>
2013-05-17 06:21:27 +0300

    PlayerGuess unit tests added.

---
+++ BullsAndCowsGame/BullsAndCows.Tests/PlayerGuessTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class PlayerGuessTests
+    {
+        [TestMethod]
+        public void NumberConstructorCorrectTest()
+        {
+            PlayerGuess number = new PlayerGuess(1, 2, 3, 4);
+            Assert.AreEqual(1, number.FirstDigit);
+            Assert.AreEqual(2, number.SecondDigit);
+            Assert.AreEqual(3, number.ThirdDigit);
+            Assert.AreEqual(4, number.FourthDigit);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NumberConstructorFirstDigitAboveLimitTest()
+        {
+            PlayerGuess number = new PlayerGuess(10, 2, 3, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NumberConstructorSecondDigitAboveLimitTest()
+        {
+            PlayerGuess number = new PlayerGuess(1, 10, 3, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NumberConstructorThirdDigitAboveLimitTest()
+        {
+            PlayerGuess number = new PlayerGuess(1, 2, 10, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NumberConstructorFourthDigitAboveLimitTest()
+        {
+            PlayerGuess number = new PlayerGuess(1, 2, 3, 10);
+        }
+
+        [TestMethod]
+        public void TryToParseMethodCorrectTest()
+        {
+            PlayerGuess expected = new PlayerGuess(5, 6, 7, 8);
+            Assert.AreEqual(expected, PlayerGuess.TryToParse("5678"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TryToParseMethodIncorrectInputTest()
+        {
+            PlayerGuess.TryToParse("wrong");
+        }
+    }
+}
